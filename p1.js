68a2fcdec4a0fb1b51b07bdad2f8d56110a76f33
Anshaj Khare <anshaj13.ugcs20@iiitranchi.ac.in>
2021-09-11 18:44:18 +0530

    Initial Commit

---
+++ p1.js
@@ -0,0 +1,66 @@
+let btn1 = document.getElementById('btn1');
+btn1.addEventListener('click', play1);
+function play1() {
+    var audio = new Audio('audio/clap.wav');
+    audio.play();
+}
+let btn2 = document.getElementById('btn2');
+btn2.addEventListener('click', play2);
+function play2() {
+    var audio = new Audio('audio/drum.wav');
+    audio.play();
+}
+let btn3 = document.getElementById('btn3');
+btn3.addEventListener('click', play3);
+function play3() {
+    var audio = new Audio('audio/flute.wav');
+    audio.play();
+}
+let btn4 = document.getElementById('btn4');
+btn4.addEventListener('click', play4);
+function play4() {
+    var audio = new Audio('audio/guitar.wav');
+    audio.play();
+}
+let btn5 = document.getElementById('btn5');
+btn5.addEventListener('click', play5);
+function play5() {
+    var audio = new Audio('audio/siren.wav');
+    audio.play();
+}
+let btn6 = document.getElementById('btn6');
+btn6.addEventListener('click', play6);
+function play6() {
+    var audio = new Audio('audio/horn.wav');
+    audio.play();
+}
+let btn7 = document.getElementById('btn7');
+btn7.addEventListener('click', play7);
+function play7() {
+    var audio = new Audio('audio/gun.wav');
+    audio.play();
+}
+let btn8 = document.getElementById('btn8');
+btn8.addEventListener('click', play8);
+function play8() {
+    var audio = new Audio('audio/bike.wav');
+    audio.play();
+}
+let btn9 = document.getElementById('btn9');
+btn9.addEventListener('click', play9);
+function play9() {
+    var audio = new Audio('audio/coin.wav');
+    audio.play();
+}
+let btn10 = document.getElementById('btn10');
+btn10.addEventListener('click', play10);
+function play10() {
+    var audio = new Audio('audio/robot.wav');
+    audio.play();
+}
+let btn11 = document.getElementById('btn11');
+btn11.addEventListener('click', play11);
+function play11() {
+    var audio = new Audio('audio/rewind.wav');
+    audio.play();
+}
